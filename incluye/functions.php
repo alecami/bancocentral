1cbf9101611bfb073a0a237a899a094d342d01f0
alecami <73922911+alecami@users.noreply.github.com>
2020-12-20 11:48:53 -0300

    Add files via upload

---
+++ incluye/functions.php
@@ -0,0 +1,18 @@
+<?php 
+	date_default_timezone_set('America/Argentina/Buenos_Aires'); 
+	function fechaC(){
+		$mes = array("","Enero", 
+					  "Febrero", 
+					  "Marzo", 
+					  "Abril", 
+					  "Mayo", 
+					  "Junio", 
+					  "Julio", 
+					  "Agosto", 
+					  "Septiembre", 
+					  "Octubre", 
+					  "Noviembre", 
+					  "Diciembre");
+		return date('d')." de ". $mes[date('n')] . " de " . date('Y');
+	}
+ ?>
